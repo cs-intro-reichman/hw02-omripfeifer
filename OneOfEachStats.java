87039e00a92dc37282b4e98daa986c286a264a84
omripfeifer <154254204+omripfeifer@users.noreply.github.com>
2023-12-27 21:02:36 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -2,8 +2,9 @@ import java.util.Random;
 
 public class OneOfEachStats {
     public static void main(String[] args) {
-        int t = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[0]);
         int seed = Integer.parseInt(args[1]);
+        Random generator = new Random (seed);
         // number of familys with 2 k
         int k2 = 0;
         // number of familys with 3k 
@@ -19,12 +20,12 @@ public class OneOfEachStats {
         // avg family
         double avg = 0;
 // loop until i = t
-        for (int i = 1; i <= t; i++) {
+        for (int i = 1; i <= T; i++) {
             int sumb = 0;
             int sumg = 0;
 
 // until I have at least one "b" and one "g" and the total count is less than or equal to t
-            while ((sumb < 1 || sumg < 1) && (sumb + sumg <= t)) {
+            while ((sumb < 1 || sumg < 1) && (sumb + sumg <= T)) {
                 double a = generator.nextDouble();
                 if (a >= 0.5) {
                     sumb++;
@@ -55,7 +56,7 @@ public class OneOfEachStats {
             
         }
 
-        avg = sumt / t;
+        avg = sumt / T;
 
         System.out.println("Average: " + avg + " children to get at least one of each gender.");
         System.out.println("Number of families with 2 children: " + k2);
